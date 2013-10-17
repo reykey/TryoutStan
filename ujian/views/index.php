8d12acec76ba43b20503693716c1b64950c32e9e
Ricki Iqbal <iqbalricki@gmail.com>
2013-10-17 17:37:25 +0700

    Modul Ujian

---
+++ ujian/views/index.php
@@ -0,0 +1,43 @@
+<!-- tampilan modul soal mulai dari sini -->
+          <div class="content">
+            <h2>Try Out Online STAN</h2>
+          </div>
+          
+          <div class="content bg-white">
+            <div class="row-fluid">
+              <div class="span12">
+
+                <!-- ini mulai soal ditampilkan -->
+                <ul class="quiz">
+                  <li class="instruction">
+                    <h4>Sinonim</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, dolore repellat animi culpa dicta ut veritatis accusantium nulla corrupti mollitia aut optio ratione incidunt voluptate reiciendis voluptas maiores aspernatur suscipit.</p>
+                  </li>
+                  <li>
+
+                    
+
+
+                    <span class="number label label-success">1</span>
+                    <div class="question">
+                      Mulai
+                    </div>
+                    <span class="number label label-success">2</span>
+                    <div class="question">
+                      Mulai
+                    </div>
+                    <span class="number label label-success">3</span>
+                    <div class="question">
+                      Mulai
+                    </div>
+                  </li>
+
+                  <li>
+                    <h4>Aturan Main :</h4>
+                    <p></p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <!-- tampilan modul soal berakhir disini -->
